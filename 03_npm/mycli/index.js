51f7a269d19f47f9075c4f55a774aca66b7af434
Mario Lazzari <mario.lazzari@gmail.com>
2023-12-29 14:49:19 +0100

    Done lesson 60

--- 03_npm/mycli/index.js
+++ 03_npm/mycli/index.js
@@ -1,1 +1,22 @@
-console.log("cli");
+#!/usr/bin/env node
+const inquirer = require("inquirer");
+// const yargs = require("yargs");
+
+// const { argv } = yargs(process.argv);
+
+const printFiveNames = async name => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const pokemon = await res.json();
+  const moves = pokemon.moves.map(({ move }) => move.name);
+
+  console.log(moves.slice(0, 5));
+};
+
+const prompt = inquirer.createPromptModule();
+prompt({
+  type: "input",
+  name: "pokemon",
+  message: "Enter pokemon name",
+}).then(answers => answers.pokemon);
+
+printFiveNames(argv.pokemon);
